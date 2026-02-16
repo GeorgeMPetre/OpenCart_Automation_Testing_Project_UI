afee29f74929271b7d65c4efded13516ddfb417d
George Petre <george.petre23@gmail.com>
2026-02-16 19:15:29 +0000

    Add GitHub Actions

--- tests/test_ci_smoke.py
+++ tests/test_ci_smoke.py
@@ -1,13 +1,11 @@
-from urllib import request
-
 import pytest
 from utils.soft_assert import SoftAssert
 
 
-@pytest.mark.ui
 @pytest.mark.ci
 @pytest.mark.smoke
-def test_soft_assert_basic(driver):
-    soft = SoftAssert(driver, request)
+def test_ci_soft_assert_no_driver():
+    soft = SoftAssert(driver=None, request=None)
     soft.assert_true(True, "Expected True")
     soft.assert_equal("a", "a", "Expected a")
+    soft.assert_all()
